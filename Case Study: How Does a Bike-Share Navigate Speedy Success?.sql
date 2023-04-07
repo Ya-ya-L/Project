7719e5301fc020b88c73f367322d57c20e587e62
Ya-ya-L <129432367+Ya-ya-L@users.noreply.github.com>
2023-04-07 11:03:31 -0700

    Update Case Study: How Does a Bike-Share Navigate Speedy Success?.sql

--- Case Study: How Does a Bike-Share Navigate Speedy Success?.sql
+++ Case Study: How Does a Bike-Share Navigate Speedy Success?.sql
@@ -1,31 +1,45 @@
-# Deliverables
-In order to answer the key business questions, you will follow the steps of the data analysis process: **ask, prepare, process, analyze, share, and act**.
+-- Deliverables
+-- In order to answer the key business questions, you will follow the steps of the data analysis process: **ask, prepare, process, analyze, share, and act**.
 
-You will produce a report with the following deliverables:
+/* 
+ * 
+ * 
+ * 
+ * 
+ *  
+*/
 
-1. A clear statement of the business task
-2. A description of all data sources used
-3. Documentation of any cleaning or manipulation of data
-4. A summary of your analysis
-5. Supporting visualizations and key findings
-6. Your top three recommendations based on your analysis
+-- Ask
+/* 
+ * Three questions will guide the future marketing program:
+ * 1. How do annual members and casual riders use Cyclistic bikes differently?
+ * 2. Why would casual riders buy Cyclistic annual memberships?
+ * 3. How can Cyclistic use digital media to influence casual riders to become members?
+ *  
+*/
 
-## Ask
-Three questions will guide the future marketing program:
+-- Tasks
+/* 
+ * 1. Identify the business task: The ultimate business goal is to increase the amount of casual riders. The business task is to analyze previous user data to understand the key preferences and differences between casual riders and members. Then, improve the future marketing strategies and programs based on the findings.
+ * 
+ * 2. Consider key stakeholders:
+ * Lily Moreno: The director of marketing and your manager. Moreno is responsible for the development of campaigns and initiatives to promote the bike-share program. These may include email, social media, and other channels.
+ * Cyclistic marketing analytics team: A team of data analysts who are responsible for collecting, analyzing, and reporting data that helps guide Cyclistic marketing strategy. You joined this team six months ago and have been busy learning about Cyclistic’s mission and business goals — as well as how you, as a junior data analyst, can help Cyclistic achieve them.
+ * Cyclistic executive team: The notoriously detail-oriented executive team will decide whether to approve the recommended marketing program.
+ *
+ *
+ *
+*/
+/*
+ *
+ *
 
-1. How do annual members and casual riders use Cyclistic bikes differently?
-2. Why would casual riders buy Cyclistic annual memberships?
-3. How can Cyclistic use digital media to influence casual riders to become members?
 
-Tasks:
-1. Identify the business task: The ultimate business goal is to increase the amount of casual riders The business task is to analyze previous user data to understand the key preferences and differences between casual riders and members. Then, improve the future marketing strategies and programs based on the findings.
-2. Consider key stakeholders:
+● 
 
-● Lily Moreno: The director of marketing and your manager. Moreno is responsible for the development of campaigns and initiatives to promote the bike-share program. These may include email, social media, and other channels.
+● 
 
-● Cyclistic marketing analytics team: A team of data analysts who are responsible for collecting, analyzing, and reporting data that helps guide Cyclistic marketing strategy. You joined this team six months ago and have been busy learning about Cyclistic’s mission and business goals — as well as how you, as a junior data analyst, can help Cyclistic achieve them.
-
-● Cyclistic executive team: The notoriously detail-oriented executive team will decide whether to approve the recommended marketing program.
+● 
 
 ## Prepare
 
@@ -51,9 +65,9 @@ FROM
  `practice-gda-377022.cyclistic.INFORMATION_SCHEMA.COLUMNS`
 GROUP BY
  1;
- -- Now I know the column names are consistent among the 14 tables
+ -- Now I know the column names are consistent among the 14 tables.
 
---Combine the 12 table together for further analysis
+-- Combine the 12 table together for further analysis
 SELECT *
 FROM  `practice-gda-377022.cyclistic.202201`
 UNION ALL
@@ -447,7 +461,7 @@ FROM (
 GROUP BY ride_id
 ORDER BY duration DESC;
 
---Create a table for further analysis with weekday, and extract day, month, start hour, and minute duration.
+-- Create a table for further analysis with weekday, and extract day, month, start hour, and minute duration.
 SELECT 
   ride_id,
   DATE(started_at) AS day,
@@ -458,5 +472,5 @@ SELECT
   member_casual
 FROM `practice-gda-377022.cyclistic.2022`
 ORDER BY day, ride_id;
---Saved as '2022weekday'
+-- Saved as '2022weekday'
 
