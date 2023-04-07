4e8554b8dd34662035cb3b079a55bb1ad3008785
Ya-ya-L <129432367+Ya-ya-L@users.noreply.github.com>
2023-04-07 15:52:42 -0700

    Update Step2-Analyze Data.sql

--- Step2-Analyze Data.sql
+++ Step2-Analyze Data.sql
@@ -42,8 +42,8 @@ member_casual|rideable_type|num_ride|
        casual|  docked_bike|  177474|
 
 
--- I observe that members like classic bike (1,709,744) more than electric bike (1,635,908) and they've never used docked bike.
--- However, casual users like electric bike (1,253,071) more than classic bike (891,446) and docked bike (177,474).
+-- I observe that members like classic bike (1,709,743) more than electric bike (1,635,897) and they've never used docked bike.
+-- However, casual users like electric bike (1,253,060) more than classic bike (891,443) and docked bike (177,474).
 
 
 
@@ -65,32 +65,32 @@ ORDER BY total_trips DESC;
 
 -- Result:
 
-member_casual|        month|total_trips|
+member_casual|month        |total_trips|
 -------------+-------------+------------
-       member|       August|     427000|
-       member|         July|     417426|
-       casual|         July|     406046|
-       member|    September|     404636|
-       member|         June|     400148|
-       casual|         June|     369044|
-       casual|       August|     358917|
-       member|          May|     354443|
-       member|      October|     349693|
-       casual|    September|     296694|
-       casual|          May|     280414|
-       member|        April|     244832|
-       member|     November|     236947|
-       casual|      October|     208988|
-       member|        March|     194160|
-       member|     December|     136912|
-       casual|        April|     126417|
-       casual|     November|     100747|
-       member|     February|      94193|
-       casual|        March|      89880|
-       member|      January|      85250|
-       casual|     December|      44894|
-       casual|     February|      21416|
-       casual|      January|      18520|
+member       |August       |427000     |
+member       |July         |417426     |
+casual       |July         |406046     |
+member       |September    |404636     |
+member       |June         |400148     |
+casual       |June         |369044     |
+casual       |August       |358917     |
+member       |May          |354443     |
+member       |October      |349693     |
+casual       |September    |296694     |
+casual       |May          |280414     |
+member       |April        |244832     |
+member       |November     |236947     |
+casual       |October      |208988     |
+member       |March        |194160     |
+member       |December     |136912     |
+casual       |April        |126417     |
+casual       |November     |100747     |
+member       |February     |94193      |
+casual       |March        |89880      |
+member       |January      |85250      |
+casual       |December     |44894      |
+casual       |February     |21416      |
+casual       |January      |18520      |
 
 -- Members did the most trips in August and the least in January.
 -- Casual users did the most trips in July and the least in January.
@@ -125,25 +125,76 @@ December     |13.0130028711     |
 
 
 --(2) Weekday and hour
+
 --Average duration for weekday
+
 SELECT
   weekday,
   AVG(duration_minute) avg_duration,
 FROM `practice-gda-377022.cyclistic.2022weekday`
 GROUP BY weekday
 ORDER BY avg_duration DESC;
--- Weekends have the longest average duration, around 24 minutes.
+
+-- Result:
+
+weekday        |avg_duration      |
+---------------+-------------------
+Sunday         |23.578776432515312|
+Saturday       |23.189435700692972|
+Friday         |18.517415516628652|
+Monday         |18.035569291077525|
+Thursday       |16.674995395592909|
+Tuesday        |16.256463842362585|
+Wednesday      |15.9621872110     |
+
+-- Weekends have the longest average duration, around 23.58 minutes.
+
+
 
 --Average duration for hour
+
 SELECT
   start_hour,
   AVG(duration_minute) avg_duration,
 FROM `practice-gda-377022.cyclistic.2022weekday`
 GROUP BY start_hour
 ORDER BY avg_duration DESC;
--- 1-3 am have the longest average duration, with 3 am 29.67, 2 am 29.61, and 1 am 27.67 minutes.
+
+-- Result:
+
+start_hour|avg_duration|
+----------+-------------
+3         |29.18636198 |
+2         |29.12466751 |
+1         |27.23924058 |
+4         |24.27335306 |
+23        |22.52502056 |
+0         |22.39177206 |
+14        |20.92658074 |
+15        |20.68023763 |
+11        |20.53922291 |
+13        |20.40674046 |
+12        |20.12165967 |
+10        |19.99855332 |
+19        |19.54170312 |
+21        |19.37077884 |
+20        |19.2380008  |
+16        |19.06292251 |
+22        |19.05048089 |
+18        |18.65642071 |
+17        |17.94228149 |
+9         |15.84718489 |
+5         |14.99105506 |
+6         |13.70854567 |
+8         |13.12464044 |
+7         |13.07447184 |
+
+-- 1-3 am have the longest average duration, with 3 am 29.18, 2 am 29.12, and 1 am 27.23 minutes.
+
+
 
 -- Analyze the preference of different user types
+
 SELECT 
   c2.member_casual,
   weekday,
@@ -158,8 +209,27 @@ GROUP BY
   start_hour,
   member_casual
 ORDER BY
-  total_trips DESC;
+  total_trips DESC
+LIMIT 10;
+
+-- Result:
+
+member_casual|weekday      |start_hour|total_trips|
+-------------+-------------+----------+-------------
+member       |Wednesday    |17        |62673      |
+member       |Tuesday      |17        |62132      |
+member       |Thursday     |17        |59861      |
+member       |Monday       |17        |56842      |
+member       |Wednesday    |18        |48816      |
+member       |Wednesday    |16        |48135      |
+member       |Thursday     |18        |48053      |
+member       |Tuesday      |16        |47901      |
+member       |Tuesday      |18        |47070      |
+member       |Thursday     |16        |47040      |
+  
 -- I observe that members preferred to ride bikes at 5 pm on weekdays, specifically Monday - Thursday.
+
+
 SELECT 
   c2.member_casual,
   weekday,
@@ -174,10 +244,29 @@ GROUP BY
   start_hour,
   member_casual
 ORDER BY
-  total_trips DESC;
--- While casual members preferred to ride bikes on weekends and in the afternoon.
+  total_trips DESC
+LIMIT 10;
+
+-- Result:
+member_casual|weekday      |start_hour|total_trips|
+-------------+-------------+----------+-------------
+casual       |Saturday     |15        |40009      |
+casual       |Saturday     |16        |38261      |
+casual       |Saturday     |17        |38094      |
+casual       |Saturday     |13        |36505      |
+casual       |Saturday     |17        |35528      |
+casual       |Saturday     |12        |34911      |
+casual       |Sunday       |15        |34452      |
+casual       |Thursday     |17        |33919      |
+casual       |Sunday       |16        |33159      |
+casual       |Sunday       |14        |32871      |
+  
+-- While casual riders preferred to ride bikes on weekends and in the afternoon.
+
+
 
 --4. Observe the duration difference between members and casual users.
+
 SELECT
   member_casual,
   MAX(duration_minute) max_duration,
@@ -185,10 +274,20 @@ SELECT
   AVG(duration_minute) average_duration
 FROM `practice-gda-377022.cyclistic.2022weekday`
 GROUP BY member_casual;
--- Members' max duration minute was 1559.9, min was 0, and the average minute was 12.71.
--- Casual users' max duration minute was 41387.25, min was 0, and the average minute was 29.24.
+
+-- Result:
+
+member_casual|max_duration|min_duration|average_duration  |
+-------------+------------+------------+-------------------
+member       |1559        |0           |12.224915203374429|
+casual       |41387       |0           |28.656158012      |
+
+-- Members' max duration minute was 1559.9, min was 0, and the average minute was 12.22.
+-- Casual users' max duration minute was 41387.25, min was 0, and the average minute was 28.65.
+
 
 --5. Explore popular areas for users.
+
 SELECT
   member_casual,
   start_station_name,
@@ -199,5 +298,21 @@ GROUP BY
   start_station_name,
   member_casual
 ORDER BY num_ss DESC;
+
+-- Result:
+
+member_casual|start_station_name                |num_ss|
+-------------+----------------------------------+-------------
+casual       |Streeter Dr & Grand Ave           |58095 |
+casual       |DuSable Lake Shore Dr & Monroe St |31862 |
+casual       |Millennium Park                   |25530 |
+casual       |Michigan Ave & Oak St             |25265 |
+member       |Kingsbury St & Kinzie St          |24937 |
+casual       |DuSable Lake Shore Dr & North Blvd|23657 |
+member       |Clark St & Elm St                 |22040 |
+member       |Wells St & Concord Ln             |21298 |
+casual       |Shedd Aquarium                    |20265 |
+member       |University Ave & 57th St          |19953 |
+
 -- Observe the popular areas for all users. Streeter Dr & Grand Ave was the most popular station for casual users;
 -- Kingsbury St & Kinzie St was the most popular station for members.
