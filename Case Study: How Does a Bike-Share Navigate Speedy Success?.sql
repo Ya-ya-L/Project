359cd49d8ba0ffc1ec72af6ff51491f56ab53900
Ya-ya-L <129432367+Ya-ya-L@users.noreply.github.com>
2023-04-07 11:13:23 -0700

    Update and rename Case Study: How Does a Bike-Share Navigate Speedy Success?.md to Case Study: How Does a Bike-Share Navigate Speedy Success?.sql

--- Case Study: How Does a Bike-Share Navigate Speedy Success?.sql
+++ Case Study: How Does a Bike-Share Navigate Speedy Success?.sql
@@ -2,44 +2,32 @@
 -- In order to answer the key business questions, you will follow the steps of the data analysis process: **ask, prepare, process, analyze, share, and act**.
 
 /* 
- * 
- * 
- * 
- * 
- *  
+
+Ask
+ 
+ Three questions will guide the future marketing program:
+ 1. How do annual members and casual riders use Cyclistic bikes differently?
+ 2. Why would casual riders buy Cyclistic annual memberships?
+ 3. How can Cyclistic use digital media to influence casual riders to become members?
+
+
+Tasks
+
+ 1. Identify the business task: The ultimate business goal is to increase the amount of casual riders.
+    The business task is to analyze previous user data to understand the key preferences and differences between casual riders and members.
+    Then, improve the future marketing strategies and programs based on the findings.
+ 2. Consider key stakeholders:
+ ● Lily Moreno:
+   The director of marketing and your manager. 
+   Moreno is responsible for the development of campaigns and initiatives to promote the bike-share program.
+   These may include email, social media, and other channels.
+ ● Cyclistic marketing analytics team: 
+   A team of data analysts who are responsible for collecting, analyzing, and reporting data that helps guide Cyclistic marketing strategy.
+   You joined this team six months ago and have been busy learning about Cyclistic’s mission and business goals — as well as how you,
+   as a junior data analyst, can help Cyclistic achieve them.
+ ● Cyclistic executive team: The notoriously detail-oriented executive team will decide whether to approve the recommended marketing program.
+
 */
-
--- Ask
-/* 
- * Three questions will guide the future marketing program:
- * 1. How do annual members and casual riders use Cyclistic bikes differently?
- * 2. Why would casual riders buy Cyclistic annual memberships?
- * 3. How can Cyclistic use digital media to influence casual riders to become members?
- *  
-*/
-
--- Tasks
-/* 
- * 1. Identify the business task: The ultimate business goal is to increase the amount of casual riders. The business task is to analyze previous user data to understand the key preferences and differences between casual riders and members. Then, improve the future marketing strategies and programs based on the findings.
- * 
- * 2. Consider key stakeholders:
- * Lily Moreno: The director of marketing and your manager. Moreno is responsible for the development of campaigns and initiatives to promote the bike-share program. These may include email, social media, and other channels.
- * Cyclistic marketing analytics team: A team of data analysts who are responsible for collecting, analyzing, and reporting data that helps guide Cyclistic marketing strategy. You joined this team six months ago and have been busy learning about Cyclistic’s mission and business goals — as well as how you, as a junior data analyst, can help Cyclistic achieve them.
- * Cyclistic executive team: The notoriously detail-oriented executive team will decide whether to approve the recommended marketing program.
- *
- *
- *
-*/
-/*
- *
- *
-
-
-● 
-
-● 
-
-● 
 
 ## Prepare
 
